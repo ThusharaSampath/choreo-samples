b17985a997739b5b5a014c12247c987f4d101425
Thushara Sampath <mkt.sampath@yahoo.com>
2024-01-02 12:23:46 +0530

    change helloworld py for testing

--- hello-world-python-task/main.py
+++ hello-world-python-task/main.py
@@ -1,1 +1,14 @@
-print("Hello World")
+import sys
+
+def main():
+  if len(sys.argv) < 2:
+    print("Usage: python app.py <name1> <name2> ... <nameN>")
+    sys.exit(1)
+
+  names = sys.argv[1:]
+  for name in names:
+    print("Hello, {}!".format(name))
+    
+
+if __name__ == "__main__":
+    main()
