2b84ffef75d090ac0fb18d19e907558df97fa7ee
Thushara Sampath <mkt.sampath@yahoo.com>
2024-02-15 17:36:07 +0530

    Update app.js

--- hello-world-nodejs-task/app.js
+++ hello-world-nodejs-task/app.js
@@ -1,1 +1,5 @@
+// process.argv contains the command-line arguments
+const args = process.argv.slice(2); // We slice the first two elements
+
 console.log('Hello World!');
+console.log('Command-line arguments:', args);
