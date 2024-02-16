f0b8b955908d95178278c992ecc0c4f7d6b20cb8
Thushara Sampath <mkt.sampath@yahoo.com>
2024-02-16 15:14:32 +0530

    delete procfile in php task

--- hello-world-php-task/index.php
+++ hello-world-php-task/index.php
@@ -1,3 +1,9 @@
 <?php
-echo "Hello World!";
+echo "Hello World!\n";
+
+// $argv contains the command-line arguments
+$args = array_slice($argv, 1); // We slice the first element
+
+echo 'Command-line arguments: ';
+print_r($args);
 ?>
