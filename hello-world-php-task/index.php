574930a3a095fd66d487f05217a16c1bb798adca
Thushara Sampath <mkt.sampath@yahoo.com>
2024-02-19 10:40:48 +0530

    Update index.php

--- hello-world-php-task/index.php
+++ hello-world-php-task/index.php
@@ -2,9 +2,7 @@
 echo "Hello World!\n";
 
 // $argv contains the command-line arguments
-$args = array_slice($argv, 1); // We slice the first element
+// $args = array_slice($argv,1); // We slice the first element
 
-echo 'Command-line arguments: ';
-print_r($args);
-exit(0);
-?>
+// echo 'Command-line arguments: ';
+// print_r($args);
