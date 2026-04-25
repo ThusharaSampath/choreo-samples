6abee9dcb60647b0215c97aa97ccc0a4480371f9
Thushara Sampath <mkt.sampath@yahoo.com>
2026-04-26 02:01:03 +0530

    go hello world random failing task

--- hello-world-go-task/main.go
+++ hello-world-go-task/main.go
@@ -2,12 +2,33 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
+	"time"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
-	for i, arg := range os.Args[1:] {
-		fmt.Println("Arg", i, ": ", arg)
+	// Seed the random number generator
+	rand.Seed(time.Now().UnixNano())
+
+	// Maximum lifetime is 10 minutes (600 seconds)
+	maxDuration := 600
+	duration := rand.Intn(maxDuration) + 1 // at least 1 second
+
+	// Randomly decide if it will fail (50% chance)
+	shouldFail := rand.Intn(2) == 0
+
+	fmt.Printf("Task started. Duration: %d seconds. Will fail: %v\n", duration, shouldFail)
+
+	for i := 1; i <= duration; i++ {
+		time.Sleep(1 * time.Second)
+		fmt.Printf("Step %d/%d: Still running...\n", i, duration)
 	}
+
+	if shouldFail {
+		fmt.Println("Task failed randomly.")
+		os.Exit(1)
+	}
+
+	fmt.Println("Task completed successfully.")
 }
